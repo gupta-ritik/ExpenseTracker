7aac4d0624b51abc94ce1957a286b86dbf900f0e
gupta-ritik <127002992+gupta-ritik@users.noreply.github.com>
2024-06-16 03:03:38 +0530

    impement initial file and folder structure of the project

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+       <main>
+          
+          {children}
+       </main>
+    );
+  }
+  
