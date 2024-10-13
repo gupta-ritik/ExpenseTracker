173d84e8185f19f81eae1e62c465f702e2005f63
Trumil Nasit <115455383+Trumilnasit@users.noreply.github.com>
2024-10-13 18:11:13 +0530

    Update Pagination.tsx
    Made some changes to this file.

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,3 +1,133 @@
+// "use client";
+
+// import Image from "next/image";
+// import { useRouter, useSearchParams } from "next/navigation";
+
+// import { Button } from "@/components/ui/button";
+// import { formUrlQuery } from "@/lib/utils";
+
+// export const Pagination = ({ page, totalPages }: PaginationProps) => {
+//   const router = useRouter();
+//   const searchParams = useSearchParams()!;
+
+//   const handleNavigation = (type: "prev" | "next") => {
+//     const pageNumber = type === "prev" ? page - 1 : page + 1;
+
+//     const newUrl = formUrlQuery({
+//       params: searchParams.toString(),
+//       key: "page",
+//       value: pageNumber.toString(),
+//     });
+
+//     router.push(newUrl, { scroll: false });
+//   };
+
+//   return (
+//     <div className="flex justify-between gap-3">
+//       <Button
+//         size="lg"
+//         variant="ghost"
+//         className="p-0 hover:bg-transparent"
+//         onClick={() => handleNavigation("prev")}
+//         disabled={Number(page) <= 1}
+//       >
+//         <Image
+//           src="/icons/arrow-left.svg"
+//           alt="arrow"
+//           width={20}
+//           height={20}
+//           className="mr-2"
+//         />
+//         Prev
+//       </Button>
+//       <p className="text-14 flex items-center px-2">
+//         {page} / {totalPages}
+//       </p>
+//       <Button
+//         size="lg"
+//         variant="ghost"
+//         className="p-0 hover:bg-transparent"
+//         onClick={() => handleNavigation("next")}
+//         disabled={Number(page) >= totalPages}
+//       >
+//         Next
+//         <Image
+//           src="/icons/arrow-left.svg"
+//           alt="arrow"
+//           width={20}
+//           height={20}
+//           className="ml-2 -scale-x-100"
+//         />
+//       </Button>
+//     </div>
+//   );
+// };
+
+// "use client";
+
+// import Image from "next/image";
+// import { useRouter, useSearchParams } from "next/navigation";
+
+// import { Button } from "@/components/ui/button";
+// import { formUrlQuery } from "@/lib/utils";
+
+// export const Pagination = ({ page, totalPages }: PaginationProps) => {
+//   const router = useRouter();
+//   const searchParams = useSearchParams()!;
+
+//   const handleNavigation = (type: "prev" | "next") => {
+//     const pageNumber = type === "prev" ? page - 1 : page + 1;
+
+//     const newUrl = formUrlQuery({
+//       params: searchParams.toString(),
+//       key: "page",
+//       value: pageNumber.toString(),
+//     });
+
+//     router.push(newUrl, { scroll: false });
+//   };
+
+//   return (
+//     <div className="flex justify-between gap-3">
+//       <Button
+//         size="lg"
+//         variant="ghost"
+//         className="p-0 hover:bg-transparent"
+//         onClick={() => handleNavigation("prev")}
+//         disabled={Number(page) <= 1}
+//       >
+//         <Image
+//           src="/icons/arrow-left.svg"
+//           alt="arrow"
+//           width={20}
+//           height={20}
+//           className="mr-2"
+//         />
+//         Prev
+//       </Button>
+//       <p className="text-14 flex items-center px-2">
+//         {page} / {totalPages}
+//       </p>
+//       <Button
+//         size="lg"
+//         variant="ghost"
+//         className="p-0 hover:bg-transparent"
+//         onClick={() => handleNavigation("next")}
+//         disabled={Number(page) >= totalPages}
+//       >
+//         Next
+//         <Image
+//           src="/icons/arrow-left.svg"
+//           alt="arrow"
+//           width={20}
+//           height={20}
+//           className="ml-2 -scale-x-100"
+//         />
+//       </Button>
+//     </div>
+//   );
+// };
+
 "use client";
 
 import Image from "next/image";
@@ -30,10 +160,11 @@ export const Pagination = ({ page, totalPages }: PaginationProps) => {
         className="p-0 hover:bg-transparent"
         onClick={() => handleNavigation("prev")}
         disabled={Number(page) <= 1}
+        aria-label="Go to the previous page"
       >
         <Image
           src="/icons/arrow-left.svg"
-          alt="arrow"
+          alt="Previous"
           width={20}
           height={20}
           className="mr-2"
@@ -49,11 +180,12 @@ export const Pagination = ({ page, totalPages }: PaginationProps) => {
         className="p-0 hover:bg-transparent"
         onClick={() => handleNavigation("next")}
         disabled={Number(page) >= totalPages}
+        aria-label="Go to the next page"
       >
         Next
         <Image
           src="/icons/arrow-left.svg"
-          alt="arrow"
+          alt="Next"
           width={20}
           height={20}
           className="ml-2 -scale-x-100"
